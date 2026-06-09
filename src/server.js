2e6aeda6187ed899d22a6c3cd6189a890e53a073
WinifredEsinam-pro <kokosuwinifred@gmail.com>
2026-06-09 10:20:04 +0000

    fix: fix server.js imports for Render deployment

--- src/server.js
+++ src/server.js
@@ -1,10 +1,7 @@
 import 'dotenv/config'
-import './jobs/mint-coins.js'
 import app from './app.js'
 import env from './config/env.js'
 
 app.listen(env.PORT, () => {
   console.log(`Quorum API running on port ${env.PORT} [${env.NODE_ENV}]`)
 })
-
-const logger = require('./config/logger')
