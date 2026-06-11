106a5c5a0ecf74bc1323b5119c9d7add441a3aad
Niiijima <qagwu11@gmail.com>
2026-06-11 16:31:52 +0100

    api

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import { login, register } from '../admin/auth.controller.js'; 
+
+import { login, register } from './auth.controller.js'; 
 
 const router = Router();
 
